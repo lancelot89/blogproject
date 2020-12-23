382e200bd88574ba9c27fcfe36908d509fdb172b
lancelot89 <takafumi@fukuchitakafuminoMacBook-Air.local>
2020-12-23 21:25:37 +0900

    static add

--- blogapp/models.py
+++ blogapp/models.py
@@ -19,6 +19,12 @@ class Profile(models.Model):
 
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     bio = models.TextField()
+    profile_img = models.ImageField(
+        null=True, blank=True, upload_to='images/profile/')
+    website_url = models.CharField(max_length=250, null=True, blank=True)
+    facebook_url = models.CharField(max_length=250, null=True, blank=True)
+    twitter_url = models.CharField(max_length=250, null=True, blank=True)
+    instagram_url = models.CharField(max_length=250, null=True, blank=True)
 
     def __str__(self):
         return str(self.user)
